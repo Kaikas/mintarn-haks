6a02c8308e76a02d75ca88ab78a8afa798b8ec82
Max Riechelmann <max.riechelmann@googlemail.com>
2021-06-24 10:43:18 +0200

    fix: subcategories

--- tools/placeable_parser.py
+++ tools/placeable_parser.py
@@ -7,5 +7,7 @@ data = json.load(f)
 
 for i in data["MAIN"]["value"]:
   print(i["DELETE_ME"]["value"])
-
+  if "LIST" in i:
+    for j in i["LIST"]["value"]:
+      print(" - " + j["DELETE_ME"]["value"])
 f.close()

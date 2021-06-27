a110ca6af1672c4034bf2416be189eef30759b37
kaikas <max.riechelmann@googlemail.com>
2021-06-27 17:01:22 +0200

    fix: hopefully fixed tlk

--- tools/placeable_parser.py
+++ tools/placeable_parser.py
@@ -6,20 +6,32 @@ def value(json):
 
 def strref(json):
   if "STRREF" in json:
-    return "TLK: " + str(json["STRREF"]["value"] - 16777217)
+    return json["STRREF"]["value"] - 16777217
   else:
-    return "TLK: null"
+    return 0
 
-f = open('src/itp/placeablepal.itp.json')
+def find_itp_string(json, p_id):
+  for i in json["entries"]:
+    if i["id"] == p_id:
+      return i["text"]
+  return "null"
 
-data = json.load(f)
+placeable_file = open('src/itp/placeablepal.itp.json')
+tlk_file = open('src/tlk/mintarn.tlk.json')
 
-for i in data["MAIN"]["value"]:
-  print(value(i) + " [" + strref(i) + "]")
+p_data = json.load(placeable_file)
+t_data = json.load(tlk_file)
+
+for i in p_data["MAIN"]["value"]:
+  print(value(i) + " [TLK: " + str(strref(i)) + "]: " + find_itp_string(t_data, strref(i)))
   if "LIST" in i:
     for j in i["LIST"]["value"]:
-      print("- " + value(j) + " [" + strref(j) + "]")
+      print("- " + value(j) + " [" + str(strref(j)) + "]")
       if "LIST" in j:
         for k in j["LIST"]["value"]:
-          print("-- " + value(j) + " [" + strref(j) + "]")
-f.close()
+          print("-- " + value(j) + " [" + str(strref(j)) + "]")
+placeable_file.close()
+tlk_file.close()
+
+#16777441
+
